1c0698a5fee3fce7b244d0c23f8f41ef4a6a6a47
thaisvictoria <142370586+thaisvictoria@users.noreply.github.com>
2023-09-17 00:01:49 -0300

    Delete planeta directory

--- planeta/planeta.js
+++
@@ -1,8 +0,0 @@
-let planetas =["Terra", "Marte", "Plutão", "Vênus", "Júpiter", "Saturno"];
-
-const searchPlanet = prompt("Digite um planeta:")
-if (planetas.includes(searchPlanet)) {
-    alert("O planeta " + searchPlanet + " está na lista de planetas!");
-} else {
-    alert("Não está na ista de planetas.");
-}
